f4ed8fdb38ed331053fef2315341e35c83b6a62a
souravkumar <souravkumarswain96@gmail.com>
2026-08-13 08:45:08 +0530

    Components and pages created and react router set up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import ProductList from "./pages/ProductList"
+import Footer from './components/Footer'
+import Error from "./pages/Error"
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import ProductDetails from './pages/ProductDetails'
 
-const App = () => {
-  return (
-    <div className='text-2xl'>Hello Sourav</div>
-  )
+
+const DeliveryAppLayout = () => {
+  return <div className="min-h-screen bg-gray-950 font-sans text-white">
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </div>
 }
 
-export default App
+const application = createBrowserRouter([
+  {
+    path: '/',
+    element: <DeliveryAppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <ProductList />
+      },
+      {
+        path: '/product/:id',
+        element: <ProductDetails />
+      },
+      {
+        path: '/cart',
+        element: <Cart />
+      },
+      {
+        path: '/checkout',
+        element: <Checkout />
+      }
+    ],
+    errorElement: <Error />
+  }
+])
+
+export default function App() {
+  return <RouterProvider router={application} />
+}
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartItem = () => {
+  return (
+    <div>CartItems</div>
+  )
+}
+
+export default CartItem
