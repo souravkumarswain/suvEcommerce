8c943dec5e6d2c67367baafd754c0a5ca87a2fb3
souravkumar <souravkumarswain96@gmail.com>
2026-08-13 09:21:06 +0530

    NavBar Design done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { IoStorefrontOutline } from "react-icons/io5";
+import { PiShoppingCartLight } from "react-icons/pi";
+
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <header className='sticky top-0 bg-gray-950/95 backdrop-blur-md text-white shadow-2xl 
+    shadow-gray-950/70 border-b border-orange-900'>
+        <div className='container mx-auto px-4 py-4 flex justify-between items-center'>
+          <Link to={'/'}>
+            <div className='flex items-center space-x-3 cursor-pointer'>
+              <IoStorefrontOutline className="w-8 h-8 text-orange-400 drop-shadow-lg" />
+              <h1 className="text-4xl font-extrabold tracking-widest uppercase">SUV
+                <span className="text-orange-400">STORE</span></h1>
+            </div>
+          </Link>
+          <nav className="flex items-center flex-x-6">
+            <Link to={'/cart'}
+              className="relative p-3 bg-orange-500/10 rounded-xl hover:bg-orange-500/20 
+            transition: duration-200 border border-orange-400/50 shadow-lg cursor-pointer">
+              <PiShoppingCartLight className="w-6 h-6 text-orange-400" />
+            </Link>
+          </nav>
+        </div>
+
+      </header>
+    </>
   )
 }
 
