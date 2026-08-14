10a6e415167f652e3fca7ffdcd1ab28a70f03420
souravkumar <souravkumarswain96@gmail.com>
2026-08-14 08:05:01 +0530

    category filter UI designed

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+
+import { initialProducts } from '../data/product'
+import { IoPricetagOutline } from "react-icons/io5";
+
+
+const availableCategory = [
+  "All",
+  ...new Set(initialProducts.map(p => p.category))
+]
 
 const CategoryFilter = () => {
+  const selectedCategory = "Phone"
   return (
-    <div>CategoryFilter</div>
+    <>
+      <div className='flex flex-wrap gap-3 border-b border-gray-800 pb-6'>
+          <IoPricetagOutline className='w-5 h-5 text-orange-500 mt-2 mr-2 hidden sm:block'/>
+          {
+            availableCategory.map(c => (
+              <button  key={c} className={`px-5 py-2 text-sm font-bold rounded-full transition
+              duration-200 shadow-md hover:cursor-pointer ${selectedCategory === c ? 
+              `bg-orange-600 text-white shadow-orange-800/50` :
+              `bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-orange-400 border border-gray-700`}`}>
+                {c}
+              </button>
+            ))
+          }
+      </div>
+    </>
   )
 }
 
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SearchFilter from '../components/SearchFilter'
+import CategoryFilter from '../components/CategoryFilter'
 
 const ProductList = () => {
   return (
     <>
       <div className='container mx-auto px-4 md:px-8 pt-8'>
         <SearchFilter/>
+        <CategoryFilter/>
       </div>
     </>
   )
