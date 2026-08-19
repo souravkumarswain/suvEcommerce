0ffeb33d9e9aac21d7290232120721fa8ae49d2d
souravkumar <souravkumarswain96@gmail.com>
2026-08-19 08:07:03 +0530

    Product Card designed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { LuShoppingBasket } from "react-icons/lu";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
-    <div>ProductCard</div>
+    <>
+      <div className='bg-gray-900 rounded-2xl shadow-xl overflow-hidden flex flex-col h-full transition
+      duration-500 transform border border-gray-800 group hover:scale-[1.03] hover:shadow-orange-900/40'>
+        <Link to={'/product'}
+          className='relative cursor-pointer overflow-hidden'>
+          <img src={product.image} alt={product.name}
+            className=' w-full h-56 object-cover object-center transition duration-500 group
+          group-hover:scale-110 group-hover:opacity-90'
+          />
+          <div
+            className='absolute bottom-0 left-0 bg-orange-600/95 text-white px-5 py-2 text-xl font-extrabold
+          rounded-tr-xl shadow-lg'
+          >{product.price.toFixed(2)}</div>
+        </Link>
+        <div className='p-5 flex flex-col grow'>
+          <Link to={'/product'}>
+            <h3 className='text-2xl font-extrabold text-white mb-2 cursor-pointer hover:text-orange-400
+        transition duration-200 line-clamp-1'>
+              {product.name}
+            </h3>
+          </Link>
+          <p className='text-gray-400 text-sm mb-4 line-clamp-3'>{product.description}</p>
+          <div className='flex items-center text-xs text-gray-500 mb-4'>
+            <span className='px-3 py-1 bg-gray-800 border-gray-700 rounded-full
+          font-semibold'>{product.category}</span>
+          </div>
+          <button className='mx-auto w-full py-3 bg-orange-600 text-white
+          font-bold rounded-full shadow-lg shadow-orange-800/50 cursor-pointer hover:bg-orange-700 transition duration-300
+          flex items-center justify-center space-x-2 transform hover:ring-4 hover:ring-orange-600/50 uppercase
+          tracking-wider'>
+            <LuShoppingBasket className='w-5 h-5'/>
+            <span>Add to cart</span>
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+
 import SearchFilter from '../components/SearchFilter'
 import CategoryFilter from '../components/CategoryFilter'
 import { useCart } from '../context/CartContext'
+import ProductCard from '../components/ProductCard'
 
 const ProductList = () => {
   const {products} = useCart()
@@ -10,6 +11,13 @@ const ProductList = () => {
       <div className='container mx-auto px-4 md:px-8 pt-8'>
         <SearchFilter/>
         <CategoryFilter/>
+        <h2 className='text-2xl font-extrabold mx-auto px-4 md:px-4 pt-4'>
+          Featured Gear ({products.length} Items)</h2>
+          <div className='mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-center items-center'>
+            {
+              products.map((product,index) => <ProductCard key = {index} product={product}></ProductCard>)
+            }
+          </div>
       </div>
     </>
   )
