5bbe4333a83cf064c63823919dc76d01cda218fa
souravkumar <souravkumarswain96@gmail.com>
2026-08-30 11:21:27 +0530

    Footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
 
 const Footer = () => {
+  const year = new Date().getFullYear()
+  
   return (
-    <div>Footer</div>
+    <div>
+      <footer className="bg-gray-950 text-gray-500 text-center
+      p-6 border-t border-gray-800">
+        <p className="text-sm">
+          &copy; {year}, All rights reserved |    
+          <span className="text-orange-400 text-sm ml-2">
+             SUV e-Commerce Solution
+          </span>
+        </p>
+      </footer>
+    </div>
   )
 }
 
